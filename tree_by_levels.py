ff2c5979288a508d9d859e4602c1123608555444
Gaurund <pavel.makushev@gmail.com>
2025-09-19 15:51:47 +0300

    Tree by levels

--- tree_by_levels.py
+++ tree_by_levels.py
@@ -22,35 +22,16 @@ class Node:
         self.right = R
         self.value = n
 
-
-def walk(node: Node):
-    if node is None:
-        return ()
-    node_list = [node.value, walk(node.left), walk(node.right)]
-    return node_list
-
-
-
 def tree_by_levels(node: Node):
-    node_list = walk(node)
-    rec_print(node_list)
-    return node_list
-    
-def rec_print(node_list):
-    if isinstance(node_list, int):
-        print(node_list, end=" > ")
-    else:
-        for l in node_list:
-            rec_print(l)
+    result = list()
+    queue = [node]
+    while queue:
+        n = queue.pop(0)
+        if n is not None:
+            result.append(n.value)
+            queue.append(n.left)
+            queue.append(n.right)
+    return result if not node is None else []
 
-# unittest.main()
+unittest.main()
 
-t = tree_by_levels(
-                Node(
-                    Node(None, Node(None, None, 4), 2),
-                    Node(Node(None, None, 5), Node(None, None, 6), 3),
-                    1,
-                )
-            )
-
-print(t)
