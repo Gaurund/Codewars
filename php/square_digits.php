520bb286b720d02c726a001b3f76aaa8ad298a6b
Gaurund <pavel.makushev@gmail.com>
2025-09-28 16:58:26 +0300

    String to array

--- php/square_digits.php
+++ php/square_digits.php
@@ -12,3 +12,8 @@ function square_digits(int $num): int
 
   return intval($result);
 }
+
+function square_dig_implode(int $num): int
+{
+  return (int)implode(array_map(function($e){return pow($e, 2);}, str_split($num)));
+}
