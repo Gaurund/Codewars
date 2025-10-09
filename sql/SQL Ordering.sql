2ae7a9cc81e4c86ed26b3942d2084e67cbeb6465
Gaurund <pavel.makushev@gmail.com>
2025-10-09 22:51:50 +0300

    SQL ordering

---
+++ sql/SQL Ordering.sql
@@ -0,0 +1,2 @@
+-- Your task is to sort the information in the provided table 'companies' by number of employees (high to low). Returned table should be in the same format as provided:
+SELECT * FROM companies ORDER BY employees DESC;
