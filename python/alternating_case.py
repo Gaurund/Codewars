048dc61bfa38aaea4984244038be0453fb602bcc
Gaurund <pavel.makushev@gmail.com>
2025-10-28 11:51:56 +0300

    Clumsy solution for alternatine case

---
+++ python/alternating_case.py
@@ -0,0 +1,56 @@
+"""
+altERnaTIng cAsE <=> ALTerNAtiNG CaSe
+Define String.prototype.toAlternatingCase (or a similar function/method such as 
+to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; 
+see the initial solution for details) such that each lowercase letter becomes 
+uppercase and each uppercase letter becomes lowercase. 
+"""
+
+import codewars_test as test
+
+def to_alternating_case(string):
+    result = ""
+    s_list = list(string)
+    for letter in s_list:
+        if letter.isupper():
+            l = letter.lower()
+        else:
+            l = letter.upper()
+        result = result + l
+    return result 
+
+@test.describe("Basic tests")
+def test_bacics():
+    @test.it("should work for fixed tests (provided in the description)")
+    def test_fixed():
+        test.assert_equals(to_alternating_case("hello world"), "HELLO WORLD")
+        test.assert_equals(to_alternating_case("HELLO WORLD"), "hello world")
+        test.assert_equals(to_alternating_case("hello WORLD"), "HELLO world")
+        test.assert_equals(to_alternating_case("HeLLo WoRLD"), "hEllO wOrld")
+        test.assert_equals(to_alternating_case("12345"), "12345")
+        test.assert_equals(to_alternating_case("1a2b3c4d5e"), "1A2B3C4D5E")
+        test.assert_equals(to_alternating_case("String.prototype.toAlternatingCase"), "sTRING.PROTOTYPE.TOaLTERNATINGcASE")
+        test.assert_equals(to_alternating_case(to_alternating_case("Hello World")), "Hello World")
+        
+        
+    @test.it("should work for the title of this Kata")
+    def test_title():
+        title = "altERnaTIng cAsE"
+        title = to_alternating_case(title)
+        test.assert_equals(title, "ALTerNAtiNG CaSe")
+        title = to_alternating_case(title)
+        test.assert_equals(title, "altERnaTIng cAsE")
+        title = to_alternating_case(title)
+        test.assert_equals(title, "ALTerNAtiNG CaSe")
+        title = to_alternating_case(title)
+        test.assert_equals(title, "altERnaTIng cAsE")
+        title = "altERnaTIng cAsE <=> ALTerNAtiNG CaSe"
+        title = to_alternating_case(title)
+        test.assert_equals(title, "ALTerNAtiNG CaSe <=> altERnaTIng cAsE")
+        title = to_alternating_case(title)
+        test.assert_equals(title, "altERnaTIng cAsE <=> ALTerNAtiNG CaSe")
+        title = to_alternating_case(title)
+        test.assert_equals(title, "ALTerNAtiNG CaSe <=> altERnaTIng cAsE")
+        title = to_alternating_case(title)
+        test.assert_equals(title, "altERnaTIng cAsE <=> ALTerNAtiNG CaSe")
+
