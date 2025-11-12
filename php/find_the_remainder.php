97d2bbc4ebd070ac1933f92f9aa272b972e3fc2b
Gaurund <pavel.makushev@gmail.com>
2025-11-12 10:56:30 +0300

    Find the remainder

---
+++ php/find_the_remainder.php
@@ -0,0 +1,35 @@
+<!-- 
+Write a function that accepts two integers and returns the remainder of dividing the larger value by the smaller value.
+
+Division by zero should return an empty value.
+
+Examples:
+n = 17
+m = 5
+result = 2 (remainder of `17 / 5`)
+
+n = 13
+m = 72
+result = 7 (remainder of `72 / 13`)
+
+n = 0
+m = -1
+result = 0 (remainder of `0 / -1`)
+
+n = 0
+m = 1
+result - division by zero (refer to the specifications on how to handle this in your language) 
+-->
+
+<?php
+
+function remainder(...$args)
+{
+    sort($args);
+    if ($args[0] == 0) {
+        return null;
+    }
+    return $args[1] % $args[0];
+}
+
+echo remainder(5, 17);
