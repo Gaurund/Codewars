c3d931ce2d2e2cfabe4e66d76185592db8b2f0a7
Gaurund <pavel.makushev@gmail.com>
2025-12-01 11:15:29 +0300

    Convert a Boolean to a String

---
+++ php/Convert a Boolean to a String.php
@@ -0,0 +1,9 @@
+<!-- Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given. -->
+
+<?php
+  
+function booleanToString(bool $b): string {
+  return $b ? "true" : "false";
+}
